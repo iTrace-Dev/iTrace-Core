e00973b53996ec56b22a6a81545e7dc2907d94c5
Drew Guarnera <dguarner@kent.edu>
2018-10-14 20:40:34 -0400

    Add basic singleton for handling gazes

---
+++ itrace_core/GazeHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iTrace_Core
+{
+    public sealed class GazeHandler
+    {
+        private static readonly Lazy<GazeHandler> Singleton =
+            new Lazy<GazeHandler>(() => new GazeHandler());
+
+        public static GazeHandler Instance { get { return Singleton.Value ; } }
+
+        private GazeHandler()
+        {
+        }
+    }
+}
