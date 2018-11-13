5163203d49242d9ace2a74c3278d6efce47f5f07
vzyrianov <vzyriano@kent.edu>
2018-11-13 12:58:19 -0500

    Integrated ShowResultsAndClose function to be used in TobiiTracker

--- itrace_core/TobiiTracker.cs
+++ itrace_core/TobiiTracker.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace iTrace_Core
 {
@@ -55,14 +56,36 @@ namespace iTrace_Core
             }
             
             Calibration.LeaveCalibrationMode();
-            calibrationWindow.Close();
+
+
+            List<Point> leftEyePoints = new List<Point>();
+            List<Point> rightEyePoints = new List<Point>();
+
+            for (int i = 0; i < calibrationResult.CalibrationPoints.Count; ++i)
+            {
+                for(int j = 0; j < calibrationResult.CalibrationPoints[i].CalibrationSamples.Count; ++j)
+                {
+                    leftEyePoints.Add(new Point(
+                            calibrationResult.CalibrationPoints[i].CalibrationSamples[j].LeftEye.PositionOnDisplayArea.X * calibrationWindow.ActualWidth,
+                            calibrationResult.CalibrationPoints[i].CalibrationSamples[j].LeftEye.PositionOnDisplayArea.Y * calibrationWindow.Height
+                        ));
+
+                    rightEyePoints.Add(new Point(
+                            calibrationResult.CalibrationPoints[i].CalibrationSamples[j].RightEye.PositionOnDisplayArea.X * calibrationWindow.ActualWidth,
+                            calibrationResult.CalibrationPoints[i].CalibrationSamples[j].RightEye.PositionOnDisplayArea.Y * calibrationWindow.Height
+                        ));
+                }
+            }
+
+
+            calibrationWindow.ShowResultsAndClose(leftEyePoints.ToArray(), rightEyePoints.ToArray());
         }
 
         private void CalibrationWindow_OnCalibrationPointReached(object sender, CalibrationPointReachedEventArgs e)
         {
             Tobii.Research.NormalizedPoint2D point = new Tobii.Research.NormalizedPoint2D(
-                (float) (e.CalibrationPoint.X / calibrationWindow.ActualWidth),
-                (float) (e.CalibrationPoint.Y / calibrationWindow.ActualHeight)
+                    (float) (e.CalibrationPoint.X / calibrationWindow.ActualWidth),
+                    (float) (e.CalibrationPoint.Y / calibrationWindow.ActualHeight)
                 );
             Calibration.CollectData(point);
         }
