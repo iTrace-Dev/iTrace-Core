38de5cbfd70cecffb6f45a233c3dbacc43732782
Vlas Zyrianov <vzyriano@kent.edu>
2018-12-13 20:03:07 -0500

    Create SocketServer

---
+++ itrace_core/SocketServer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iTrace_Core
+{
+    class SocketServer
+    {
+        public void Start()
+        {
+            GazeHandler.Instance.OnGazeDataRecieved += RecieveGazeData;            
+        }
+
+        private void RecieveGazeData(object sender, GazeDataRecievedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
