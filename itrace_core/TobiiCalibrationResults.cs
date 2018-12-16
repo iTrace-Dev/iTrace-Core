d8a6536a3cd810dee9fe995ab5143cdc31bb39c0
Vlas Zyrianov <vzyriano@kent.edu>
2018-12-16 00:52:51 -0500

    Create untested SaveResults function for calibration results

--- itrace_core/TobiiCalibrationResults.cs
+++ itrace_core/TobiiCalibrationResults.cs
@@ -61,9 +61,46 @@ namespace iTrace_Core
             return calibrationResult.Status == Tobii.Research.CalibrationStatus.Failure;
         }
 
+        private String createPointSample(float leftX, float leftY, bool leftValidity, float rightX, float rightY, bool rightValidity)
+        {
+            String leftValidityString = leftValidity ? "1" : "-1";
+            String rightValidityString = rightValidity ? "1" : "-1";
+
+            return "<sample left_x=\"" + leftX.ToString() + "\" left_y=\"" + leftY.ToString() + "\" left_validity=\"" + leftValidityString + "\" right_x=\"" +
+                rightX.ToString() + "\" right_y=\"" + rightY.ToString() + "\" right_validity=\"" + rightValidityString + "\"/>";
+        }
+
         void SaveResultsToXML()
         {
+            StringBuilder calibrationResultsXML = new StringBuilder("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
 
+            calibrationResultsXML.AppendLine("<calibration status=\"1\">"); // Status should be changed?
+
+            for(int i = 0; i < calibrationResult.CalibrationPoints.Count; ++i)
+            {
+                string pointX = calibrationResult.CalibrationPoints[i].PositionOnDisplayArea.X.ToString();
+                string pointY = calibrationResult.CalibrationPoints[i].PositionOnDisplayArea.Y.ToString();
+                calibrationResultsXML.AppendLine("<point x=\"" + pointX + "\" y=\"" + pointY + "\">");
+
+                for(int j = 0; j < calibrationResult.CalibrationPoints[i].CalibrationSamples.Count; ++j)
+                {
+                    Tobii.Research.CalibrationSample sample = calibrationResult.CalibrationPoints[i].CalibrationSamples[j];
+                    calibrationResultsXML.AppendLine(createPointSample(
+                        sample.LeftEye.PositionOnDisplayArea.X,
+                        sample.LeftEye.PositionOnDisplayArea.Y,
+                        sample.LeftEye.Validity != Tobii.Research.CalibrationEyeValidity.InvalidAndNotUsed,
+                        sample.RightEye.PositionOnDisplayArea.X,
+                        sample.RightEye.PositionOnDisplayArea.Y,
+                        sample.RightEye.Validity != Tobii.Research.CalibrationEyeValidity.InvalidAndNotUsed
+                        ));
+                }
+            }
+
+            calibrationResultsXML.AppendLine("</calibration>");
+
+            string result = calibrationResultsXML.ToString();
+
+            //To do: output result to a file
         }
     }
 }
