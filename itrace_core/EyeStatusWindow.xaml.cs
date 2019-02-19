37ab6234ba449e210906a08e221d50b4f6ea88b5
Vlas Zyrianov <vzyriano@kent.edu>
2019-02-19 13:02:00 -0500

    Progress on incomplete EyeStatusWindow

--- itrace_core/EyeStatusWindow.xaml.cs
+++ itrace_core/EyeStatusWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
+using System;
 
 namespace iTrace_Core
 {
@@ -19,8 +20,8 @@ namespace iTrace_Core
         {
             InitializeComponent();
 
-            leftEyeCircle = new EllipseGeometry(new Point(400,300), 20, 20);
-            rightEyeCircle = new EllipseGeometry(new Point(500, 300), 20, 20);
+            leftEyeCircle = new EllipseGeometry(new Point(400,300), 5, 5);
+            rightEyeCircle = new EllipseGeometry(new Point(500, 300), 5, 5);
 
             this.RegisterName(registeredLeftEyeName, leftEyeCircle);
             this.RegisterName(registeredRightEyeName, rightEyeCircle);
@@ -33,6 +34,7 @@ namespace iTrace_Core
             rightEyePath.Fill = Brushes.White;
             rightEyePath.Data = rightEyeCircle;
 
+            this.KeyUp += Grid_KeyUp;
             Canvas containerCanvas = new Canvas();
             containerCanvas.Background = Brushes.Black;
             containerCanvas.Children.Add(leftEyePath);
@@ -40,9 +42,70 @@ namespace iTrace_Core
             this.Content = containerCanvas;
         }
 
+        Vector4 HomogeneousTransform(Vector4 vec, Matrix4x4 mat)
+        {
+            Vector4 result = Vector4.Transform(vec, mat);
+
+            result.X = result.X / result.W;
+            result.Y = result.Y / result.W;
+            result.Z = result.Z / result.W;
+
+            return result;
+        }
+
         public void UpdateEyePosition(Vector3 leftEyePosition, Vector3 rightEyePosition)
         {
+            Vector4 leftEyeHomogeneous = new Vector4(leftEyePosition, 1);
+            Vector4 rightEyeHomogeneous = new Vector4(rightEyePosition, 1);
+            Matrix4x4 projectionMatrix = Matrix4x4.CreatePerspectiveFieldOfView(Convert.ToSingle(Math.PI / 8), 16.0f / 9.0f, 1, 1000);
 
+            Vector4 leftEyeProjected = HomogeneousTransform(leftEyeHomogeneous, projectionMatrix);
+            Vector4 rightEyeProjected = HomogeneousTransform(rightEyeHomogeneous, projectionMatrix);
+
+            leftEyeCircle.Center = new Point(leftEyeProjected.X + 400, leftEyeProjected.Y + 400);
+            rightEyeCircle.Center = new Point(rightEyeProjected.X + 400, rightEyeProjected.Y + 400);
+        }
+
+        //Temporary for testing
+        Vector3 currentLeftEyePosition = new Vector3(10.0f, 0.0f, 5.0f);
+        Vector3 currentRightEyePosition = new Vector3(-10.0f, 0.0f, 5.0f);
+
+        private void Grid_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == System.Windows.Input.Key.W)
+            {
+                currentRightEyePosition.Z += 1.0f;
+                currentLeftEyePosition.Z += 1.0f;
+            }
+            else if (e.Key == System.Windows.Input.Key.S)
+            {
+                currentRightEyePosition.Z -= 1.0f;
+                currentLeftEyePosition.Z -= 1.0f;
+            }
+            else if (e.Key == System.Windows.Input.Key.A)
+            {
+                currentRightEyePosition.X += 10.0f;
+                currentLeftEyePosition.X += 10.0f;
+            }
+            else if (e.Key == System.Windows.Input.Key.D)
+            {
+                currentRightEyePosition.X -= 10.0f;
+                currentLeftEyePosition.X -= 10.0f;
+            }
+            else if (e.Key == System.Windows.Input.Key.R)
+            {
+                currentRightEyePosition.Y += 10.0f;
+                currentLeftEyePosition.Y += 10.0f;
+            }
+            else if (e.Key == System.Windows.Input.Key.F)
+            {
+                currentRightEyePosition.Y -= 10.0f;
+                currentLeftEyePosition.Y -= 10.0f;
+            }
+
+            Console.WriteLine(currentLeftEyePosition.ToString());
+
+            UpdateEyePosition(currentLeftEyePosition, currentRightEyePosition);
         }
     }
 }
