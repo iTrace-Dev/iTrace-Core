c534676a99fb89f4110e87d13367d0828d95a161
vzyrianov <vzyriano@kent.edu>
2019-04-16 10:11:04 -0400

    Create PreciseSystemTime class

---
+++ itrace_core/PreciseSystemTime.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace iTrace_Core
+{
+    static class PreciseSystemTime
+    {
+        [DllImport("Kernel32.dll", CallingConvention = CallingConvention.Winapi)]
+        private static extern void GetSystemTimePreciseAsFileTime(out long filetime);
+
+        public static long GetTime()
+        {
+            long time;
+            GetSystemTimePreciseAsFileTime(out time);
+            return time;
+        }
+    }
+}
