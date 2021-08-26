83dd93e772ef142ee977c1d3521c966dd12b3f02
Aaron Linnell <aaronlinnell@gmail.com>
2021-08-26 12:12:10 -0500

    SmartEye connection sequence changes.
    a successful RPC connection must be made. The realtime port on the machine running iTrace must also be successfully opened for the tracker to be considered available.

--- itrace_core/SmartEyeRPCUtils.cs
+++ itrace_core/SmartEyeRPCUtils.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -30,6 +31,78 @@ namespace iTrace_Core
         {
             this.method = methodName;
             this.isNotification = isNotif;
+        }
+
+        //Serialize command to JSON and convert to Netstring
+        public String GetNetstring()
+        {
+            String json = JsonConvert.SerializeObject(this);
+            return NetstringUtils.MakeSENetstring(json);
+        }
+    }
+
+    public class SERPCGetStateResponse : SERPC
+    {
+        public Dictionary<string, int> responseParams;
+
+        private SERPCGetStateResponse() : base("getState")
+        {
+
+        }
+
+        public int? GetState()
+        {
+            int state;
+            bool success = responseParams.TryGetValue("state", out state);
+
+            if (success)
+                return state;
+
+            return null;
+        }
+    }
+
+    public class NetstringUtils
+    {
+        //Converts raw rpc command into a netstring as defined in the Programmers Guide
+        public static String MakeSENetstring(String rpc)
+        {
+            return rpc.Length.ToString() + ":" + rpc + ",";
+        }
+
+        //Converts a netstring to a json string
+        //A netstring may look like this 16:{"memes":"dank"},
+        //Where 16 is the length of the json before it was formatted to a netstring
+        public static String TrimSENetstring(String netstring)
+        {
+            int length = 0;
+            String json = null;
+
+            for (int i = 0; i < netstring.Length; i++)
+            {
+                //Find end of length prefix
+                if (!(netstring[i] >= '0' && netstring[i] <= '9'))
+                {
+                    //Parse length prefix
+                    length = int.Parse(netstring.Substring(0, i));
+
+                    //Trim off length prefix
+                    json = netstring.Substring(i + 1);
+                    break;
+                }
+            }
+
+            if (json == null || !json.EndsWith(","))
+                throw new Exception("Tried to parse invalid netstring: " + netstring);
+
+            //Trim off trailing comma
+            json = json.Substring(0, json.Length - 1);
+
+            //Check length
+            if (json.Length != length)
+                throw new Exception("Netstring body does not match length prefix: " + netstring);
+
+            return json;
         }
     }
 }
--- itrace_core/SmartEyeTracker.cs
+++ itrace_core/SmartEyeTracker.cs
@@ -8,9 +8,6 @@ namespace iTrace_Core
 {
     class SmartEyeTracker : ITracker
     {
-        private readonly String SMARTEYE_ADDRESS = "192.169.100.45"; //Should be determined automatically when openDataStreamUDP rpc call is made
-        private readonly String SMARTEYE_SERVER = Settings.Default.smarteye_ip_address;
-        private readonly int SMARTEYE_PORT_REALTIME = Settings.Default.smarteye_ip_port; //default from SE software
         private readonly int SMARTEYE_PORT_LATENT = 5799; //TODO set to default from SE software
         private readonly int SMARTEYE_PORT_RPC = 8100; //default from SE software
 
@@ -27,28 +24,20 @@ namespace iTrace_Core
 
         public SmartEyeTracker()
         {
-            IPAddress address = IPAddress.Parse(SMARTEYE_ADDRESS);
-            IPAddress serverAddress = IPAddress.Parse(SMARTEYE_SERVER);
-            realtimeEndpoint = new IPEndPoint(address, SMARTEYE_PORT_REALTIME);
-            latentEndpoint = new IPEndPoint(serverAddress, SMARTEYE_PORT_LATENT); 
-            rpcEndpoint = new IPEndPoint(serverAddress, SMARTEYE_PORT_RPC);
-
             TrackerName = "SmartEye Tracker";
             TrackerSerialNumber = "1234";
 
+            //TODO catch parse exception?
+            IPAddress rpcAddress = IPAddress.Parse(Settings.Default.smarteye_ip_address);
+            rpcEndpoint = new IPEndPoint(rpcAddress, SMARTEYE_PORT_RPC);
+
             try
             {
-                RealtimeClient = new System.Net.Sockets.UdpClient(realtimeEndpoint);
-                LatentClient = new System.Net.Sockets.TcpClient();
-                LatentClient.Connect(latentEndpoint);
-
+                //Try to connect to the RPC server on SmartEye host machine
                 RpcClient = new System.Net.Sockets.TcpClient();
                 RpcClient.Connect(rpcEndpoint);
 
-                //TODO: Set actual tracker name and serial
-
-                //TODO: use verbatim strings, or preferrably a JSON builder
-                SendRpc(MakeSENetstring("{\"jsonrpc\":\"2.0\", \"method\":\"getState\", \"id\":0}"));
+                SendRpc(new SERPC("getState").GetNetstring());
 
                 byte[] recvBuffer = new byte[RpcClient.ReceiveBufferSize];
                 System.Net.Sockets.NetworkStream recvStream = RpcClient.GetStream();
@@ -56,43 +45,53 @@ namespace iTrace_Core
 
                 //Netstrings test
                 String response = Encoding.UTF8.GetString(recvBuffer);
-                response = TrimSENetstring(response.TrimEnd('\0'));
+                response = NetstringUtils.TrimSENetstring(response.TrimEnd('\0'));
+
+                //Does not deserialize the response correctly yet
+                SERPCGetStateResponse state = JsonConvert.DeserializeObject<SERPCGetStateResponse>(response);
 
                 Console.WriteLine("JSON response: {0}\n", response);
-
-                TrackerInit();
-            }
-            catch (System.Net.Sockets.SocketException e)
-            {
-                Console.WriteLine("SmartEye not connected: " + e);
-                RealtimeClient = null;
             }
             catch (Exception e)
             {
-                Console.WriteLine("SmartEye Connection Failed: " + e);
-                Console.WriteLine(e.ToString());
-                RealtimeClient = null;
+                Console.WriteLine("SmartEye Connection Failed, could not connect to RPC server" +
+                    " on " + rpcEndpoint.ToString() + " : " + e);
+
+                RpcClient = null;
+                return;
             }
+
+            try
+            {
+                //Own address hardcoded, need to determine at runtime
+                realtimeEndpoint = new IPEndPoint(IPAddress.Parse("192.169.100.45"), Settings.Default.smarteye_ip_port);
+                RealtimeClient = new System.Net.Sockets.UdpClient(realtimeEndpoint);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Couldn't open realtime data port (UDP) for SmartEye: " + e);
+                RealtimeClient = null;
+                return;
+            }
+
+            try
+            {
+                latentEndpoint = new IPEndPoint(IPAddress.Parse(Settings.Default.smarteye_ip_address), SMARTEYE_PORT_LATENT);
+                LatentClient = new System.Net.Sockets.TcpClient();
+                LatentClient.Connect(latentEndpoint);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Couldn't open filtered data port (TCP) for SmartEye: " + e);
+                LatentClient = null;
+            }
+
+            TrackerInit();
         }
 
         public bool TrackerFound()
         {
-            //TODO: Because this is UDP, this does not actually mean the tracker is present and ready to go
-            //Checking presence via RPC might be excessive?
-
-            if (RealtimeClient != null)
-            {
-                Console.WriteLine("Client connected");
-
-                if (LatentClient == null)
-                    Console.WriteLine("No latent data client!");
-
-                if (RpcClient == null)
-                    Console.WriteLine("No RPC connection!");
-
-                return true;
-            }
-            else { return false; }
+            return (RealtimeClient != null) && (RpcClient != null);
         }
 
         public String GetTrackerName()
@@ -105,47 +104,6 @@ namespace iTrace_Core
             return TrackerSerialNumber;
         }
 
-        //Converts raw rpc command into a netstring as defined in the Programmers Guide
-        public static String MakeSENetstring(String rpc)
-        {
-            return rpc.Length.ToString() + ":" + rpc + ",";
-        }
-
-        //Converts a netstring to a json string
-        //A netstring may look like this 16:{"memes":"dank"},
-        //Where 16 is the length of the json before it was formatted to a netstring
-        public static String TrimSENetstring(String netstring)
-        {
-            int length = 0;
-            String json = null;
-
-            for (int i = 0; i < netstring.Length; i++)
-            {
-                //Find end of length prefix
-                if ( !(netstring[i] >= '0' && netstring[i] <= '9'))
-                {
-                    //Parse length prefix
-                    length = int.Parse(netstring.Substring(0, i));
-
-                    //Trim off length prefix
-                    json = netstring.Substring(i + 1);
-                    break;
-                }
-            }
-
-            if (json == null || !json.EndsWith(","))
-                throw new Exception("Tried to parse invalid netstring: " + netstring);
-
-            //Trim off trailing comma
-            json = json.Substring(0, json.Length - 1);
-
-            //Check length
-            if (json.Length != length)
-                throw new Exception("Netstring body does not match length prefix: " + netstring);
-
-            return json;
-        }
-
         //Send RPC netstring using TCP
         public void SendRpc(String netstring)
         {
@@ -156,8 +114,7 @@ namespace iTrace_Core
 
         public void StartTracker()
         {
-            String startTrackingJson = MakeSENetstring("{\"jsonrpc\":\"2.0\", \"method\":\"startTracking\", \"id\":0}");
-            SendRpc(startTrackingJson);
+            SendRpc(new SERPC("startTracking").GetNetstring());
 
             new System.Threading.Thread(() =>
             {
@@ -176,8 +133,7 @@ namespace iTrace_Core
 
         public void StopTracker()
         {
-            String stopTrackingJson = MakeSENetstring("{\"jsonrpc\":\"2.0\", \"method\":\"stopTracking\", \"id\":0}");
-            SendRpc(stopTrackingJson);
+            SendRpc(new SERPC("stopTracking").GetNetstring());
 
             Listen = false;
         }
@@ -185,8 +141,7 @@ namespace iTrace_Core
         public void EnterCalibration()
         {
             //Open calibration window (not working yet)
-            String enterCalibrationJson = MakeSENetstring("{\"jsonrpc\":\"2.0\", \"method\":\"calibrateGaze\", \"id\":0}");
-            SendRpc(enterCalibrationJson);
+            //SendRpc(new SERPC("calibrateGaze").GetNetstring());
 
             //TODO: receive and store response
         }
